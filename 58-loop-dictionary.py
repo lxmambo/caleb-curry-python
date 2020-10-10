48defb137627eccc51b0f46fc7c825a58f892a93
João Santos <joao_santos_2003@hotmail.com>
2020-10-10 23:50:23 +0100

    value

---
+++ 58-loop-dictionary.py
@@ -0,0 +1,11 @@
+emails = {
+    'caleb':'caleb@gmail.com',
+    'gal':'g@hotmail.com',
+    'tim':'timmy@sapo.pt',
+}
+
+for key in emails:
+    print(key,emails[key])
+
+#it's not the best solution to get the element
+#the key needs to be hashed again
