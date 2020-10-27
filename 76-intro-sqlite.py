47663300d674a21880f0a575bcd803d73e6a6162
João Santos <joao_santos_2003@hotmail.com>
2020-10-27 00:27:26 +0000

    more

--- 76-intro-sqlite.py
+++ 76-intro-sqlite.py
@@ -7,3 +7,22 @@ import sqlite3
 conn = sqlite3.connect(":memory:")
 #we open the connection to the database and give
 #a reference to it
+
+#conn = sqlite3.connect("example.db")
+#this would create a database in our parent directory
+#the file appears in the terminal folder
+
+#we have to create a cursor, a tool used to work
+#with the database and establish a reference to it
+
+c = conn.cursor()
+
+#c has methods
+#this creates the foundation to start working/create our first table
+
+#execute allows to use sql directly in to the database
+c.execute('''CREATE TABLE IF NOT EXISTS books 
+            (title TEXT, pages INTEGER)''')
+#TEXT and INTEGER are data types
+
+#To do multiple lines we use the three '''
